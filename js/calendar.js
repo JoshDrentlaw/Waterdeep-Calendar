e0f999d7bad0d75d66f9c090c81c9b05b9b7fcbe
JoshDrentlaw <joshdrentlaw@gmail.com>
2018-10-08 23:01:27 -0700

    Added es6 modules. Started implementing calendar service functionality.

--- js/calendar.js
+++ js/calendar.js
@@ -1,4 +1,9 @@
+import { nextMonth, prevMonth, currentMonth } from './calendar.service.js';
+
 (function() {
+
+    /* HANDLE ANIMATIONS */
+
     let days = document.getElementsByClassName('day');
     let hoverTimeoutId;
 
@@ -39,4 +44,28 @@
         let day = e.target;
         day.classList.remove('drop');
     }
+
+    /* CHANGE MONTH */
+
+    // days are gathered at the top for the animations.
+    let monthEl = document.getElementById('month');
+    
+    function changeMonth() {
+        let newMonth = nextMonth();
+        let holidays = newMonth.holidays;
+        let month = newMonth.currentMonth;
+
+        writeMonth(month);
+        writeHolidays(holidays)
+    }
+
+    function writeMonth(month) {
+        monthEl.innerHTML = month;
+    }
+
+    function writeHolidays(holidays) {
+        
+    }
+
+    changeMonth();
 } ());
--- js/calendar.service.js
+++ js/calendar.service.js
@@ -1,236 +1,255 @@
-(function() {
-    let calendar = [
+let calendar = {
+    "months": [
         {
-            "months": [
+            "name": "Hammer",
+            "commonName": "Deepwinter",
+            "celebrations": [
                 {
-                    "name": "Hammer",
-                    "commonName": "Deepwinter",
-                    "celebrations": [
-                        {
-                            "day": 1,
-                            "name": "Wintershield"
-                        }
-                    ]
-                },
-                {
-                    "name": "Alturiak",
-                    "commonName": "The Claw of Winter",
-                    "celebrations": [
-                        {
-                            "day": 14,
-                            "name": "The Grand Revel"
-                        }
-                    ]
-                },
-                {
-                    "name": "Ches",
-                    "commonName": "The Claw of Sunsets",
-                    "celebrations": [
-                        {
-                            "day": 1,
-                            "name": "Rhyestertide",
-                            "desc": ""
-                        },
-                        {
-                            "day": 19,
-                            "name": "Fey Day",
-                            "desc": ""
-                        },
-                        {
-                            "day": [21,22,23,24,25,26,27,28,29,30],
-                            "name": "Fleetswake",
-                            "desc": ""
-                        }
-                    ]
-                },
-                {
-                    "name": "Tarsahk",
-                    "commonName": "The Claw of Storms",
-                    "celebrations": [
-                        {
-                            "day": [1,2,3,4,5,6,7,8,9,10],
-                            "name": "Waukeentide",
-                            "desc": ""
-                        }
-                    ]
-                },
-                {
-                    "name": "Mirtul",
-                    "commonName": "The Melting",
-                    "celebrations": [
-                        {
-                            "day": [6,7,8,9],
-                            "name": "The Plowing and Running",
-                            "desc": ""
-                        }
-                    ]
-                },
-                {
-                    "name": "Kythorn",
-                    "commonName": "The Time of Flowers",
-                    "celebrations": [
-                        {
-                            "day": 1,
-                            "name": "Trolltide",
-                            "desc": ""
-                        },
-                        {
-                            "day": 14,
-                            "name": "Guildhall Day",
-                            "desc": ""
-                        },
-                        {
-                            "day": 20,
-                            "name": "Dragondown",
-                            "desc": ""
-                        }
-                    ]
-                },
-                {
-                    "name": "Flamerule",
-                    "commonName": "Summertide",
-                    "celebrations": [
-                        {
-                            "day": 1,
-                            "name": "Founder's Day",
-                            "desc": ""
-                        },
-                        {
-                            "day": [3,4,5],
-                            "name": "Sornyn",
-                            "desc": ""
-                        },
-                        {
-                            "day": 7,
-                            "name": "Lilira's Night",
-                            "desc": ""
-                        }
-                    ]
-                },
-                {
-                    "name": "Eleasis",
-                    "commonName": "Highsun",
-                    "celebrations": [
-                        {
-                            "day": 1,
-                            "name": "Ahghairon's Day",
-                            "desc": ""
-                        }
-                    ]
-                },
-                {
-                    "name": "Elient",
-                    "commonName": "The Fading",
-                    "celebrations": [
-                        {
-                            "day": 21,
-                            "name": "Brightswords",
-                            "desc": ""
-                        }
-                    ]
-                },
-                {
-                    "name": "Marpenoth",
-                    "commonName": "Leaffall",
-                    "celebrations": [
-                        {
-                            "day": 3,
-                            "name": "Day of Wonders",
-                            "desc": ""
-                        },
-                        {
-                            "day": 7,
-                            "name": "Stoneshar",
-                            "desc": ""
-                        },
-                        {
-                            "day": 10,
-                            "name": "Reign of Misrule",
-                            "desc": ""
-                        },
-                        {
-                            "day": 15,
-                            "name": "Gods' Day",
-                            "desc": ""
-                        },
-                        {
-                            "day": 30,
-                            "name": "Liar's Night",
-                            "desc": ""
-                        }
-                    ]
-                },
-                {
-                    "name": "Uktar",
-                    "commonName": "The Rotting",
-                    "celebrations": [
-                        {
-                            "day": getFullMoon(),
-                            "name": "Selune's Hallowing",
-                            "desc": ""
-                        },
-                        {
-                            "day": 20,
-                            "name": "Last Sheaf",
-                            "desc": ""
-                        }
-                    ]
-                },
-                {
-                    "name": "Nightal",
-                    "commonName": "The Drawing Down",
-                    "celebrations": [
-                        {
-                            "day": 11,
-                            "name": "Howldown",
-                            "desc": ""
-                        },
-                        {
-                            "day": 20,
-                            "name": "Simril",
-                            "desc": ""
-                        }
-                    ]
+                    "day": 1,
+                    "name": "Wintershield"
                 }
-            ],
-            "faerun holidays": [
+            ]
+        },
+        {
+            "name": "Alturiak",
+            "commonName": "The Claw of Winter",
+            "celebrations": [
                 {
-                    "name": "Midwinter",
+                    "day": 14,
+                    "name": "The Grand Revel"
+                }
+            ]
+        },
+        {
+            "name": "Ches",
+            "commonName": "The Claw of Sunsets",
+            "celebrations": [
+                {
+                    "day": 1,
+                    "name": "Rhyestertide",
                     "desc": ""
                 },
                 {
-                    "name": "Greengrass",
+                    "day": 19,
+                    "name": "Fey Day",
                     "desc": ""
                 },
                 {
-                    "name": "Midsummer",
+                    "day": [21,22,23,24,25,26,27,28,29,30],
+                    "name": "Fleetswake",
+                    "desc": ""
+                }
+            ]
+        },
+        {
+            "name": "Tarsahk",
+            "commonName": "The Claw of Storms",
+            "celebrations": [
+                {
+                    "day": [1,2,3,4,5,6,7,8,9,10],
+                    "name": "Waukeentide",
+                    "desc": ""
+                }
+            ]
+        },
+        {
+            "name": "Mirtul",
+            "commonName": "The Melting",
+            "celebrations": [
+                {
+                    "day": [6,7,8,9],
+                    "name": "The Plowing and Running",
+                    "desc": ""
+                }
+            ]
+        },
+        {
+            "name": "Kythorn",
+            "commonName": "The Time of Flowers",
+            "celebrations": [
+                {
+                    "day": 1,
+                    "name": "Trolltide",
                     "desc": ""
                 },
                 {
-                    "name": "Shieldmeet",
+                    "day": 14,
+                    "name": "Guildhall Day",
                     "desc": ""
                 },
                 {
-                    "name": "Highharvestide",
+                    "day": 20,
+                    "name": "Dragondown",
+                    "desc": ""
+                }
+            ]
+        },
+        {
+            "name": "Flamerule",
+            "commonName": "Summertide",
+            "celebrations": [
+                {
+                    "day": 1,
+                    "name": "Founder's Day",
                     "desc": ""
                 },
                 {
-                    "name": "The Feast of the Moon",
+                    "day": [3,4,5],
+                    "name": "Sornyn",
+                    "desc": ""
+                },
+                {
+                    "day": 7,
+                    "name": "Lilira's Night",
+                    "desc": ""
+                }
+            ]
+        },
+        {
+            "name": "Eleasis",
+            "commonName": "Highsun",
+            "celebrations": [
+                {
+                    "day": 1,
+                    "name": "Ahghairon's Day",
+                    "desc": ""
+                }
+            ]
+        },
+        {
+            "name": "Elient",
+            "commonName": "The Fading",
+            "celebrations": [
+                {
+                    "day": 21,
+                    "name": "Brightswords",
+                    "desc": ""
+                }
+            ]
+        },
+        {
+            "name": "Marpenoth",
+            "commonName": "Leaffall",
+            "celebrations": [
+                {
+                    "day": 3,
+                    "name": "Day of Wonders",
+                    "desc": ""
+                },
+                {
+                    "day": 7,
+                    "name": "Stoneshar",
+                    "desc": ""
+                },
+                {
+                    "day": 10,
+                    "name": "Reign of Misrule",
+                    "desc": ""
+                },
+                {
+                    "day": 15,
+                    "name": "Gods' Day",
+                    "desc": ""
+                },
+                {
+                    "day": 30,
+                    "name": "Liar's Night",
+                    "desc": ""
+                }
+            ]
+        },
+        {
+            "name": "Uktar",
+            "commonName": "The Rotting",
+            "celebrations": [
+                {
+                    "day": getFullMoon(),
+                    "name": "Selune's Hallowing",
+                    "desc": ""
+                },
+                {
+                    "day": 20,
+                    "name": "Last Sheaf",
+                    "desc": ""
+                }
+            ]
+        },
+        {
+            "name": "Nightal",
+            "commonName": "The Drawing Down",
+            "celebrations": [
+                {
+                    "day": 11,
+                    "name": "Howldown",
+                    "desc": ""
+                },
+                {
+                    "day": 20,
+                    "name": "Simril",
                     "desc": ""
                 }
             ]
         }
-    ];
+    ],
+    "faerun holidays": [
+        {
+            "name": "Midwinter",
+            "desc": ""
+        },
+        {
+            "name": "Greengrass",
+            "desc": ""
+        },
+        {
+            "name": "Midsummer",
+            "desc": ""
+        },
+        {
+            "name": "Shieldmeet",
+            "desc": ""
+        },
+        {
+            "name": "Highharvestide",
+            "desc": ""
+        },
+        {
+            "name": "The Feast of the Moon",
+            "desc": ""
+        }
+    ]
+};
 
-    // Selune's Hallowing occurs on the full moon.
-    // It takes place in the month of Uktar, which
-    //   lines up with November.
-    // The calendar will use the full moon of the
-    //   November of the year that corrisponds
-    //   to the current Dale Reckoning year
-    //   of the calendar.
-    // i.e. If DR = 1491, GET full moon for Nov. 1491
-    function getFullMoon() {
+// Selune's Hallowing occurs on the full moon.
+// It takes place in the month of Uktar, which
+//   lines up with November.
+// The calendar will use the full moon of the
+//   November of the year that corrisponds
+//   to the current Dale Reckoning year
+//   of the calendar.
+// i.e. If DR = 1491, GET full moon for Nov. 1491
+function getFullMoon() {
 
+}
+
+// This will be a call to the localStorage service eventually.
+let currentMonth = "Hammer";
+
+export function nextMonth() {
+    let holidays = getHolidays(currentMonth);
+    return {
+        holidays,
+        currentMonth
+    };
+}
+
+export function prevMonth() {
+
+}
+
+function getHolidays(month) {
+    for (let match of calendar.months) {
+        if (month == match.name) {
+            return match.celebrations;
+        }
     }
-} ());
+}
