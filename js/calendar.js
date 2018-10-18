2d68a510856b96b4781e37077980b2fb96b3df10
JoshDrentlaw <joshdrentlaw@gmail.com>
2018-10-18 08:39:42 -0700

    Calendar shows major Faerun holidays. Everything still needs descriptions.

--- js/calendar.js
+++ js/calendar.js
@@ -34,14 +34,19 @@ import { month, nextMonth, prevMonth } from './calendar.service.js';
     // Changes the month and adds holidays to it
     function renderMonth(e) {
         // Get new month from service.
+        // renderMonth is called once when the script loads outside of an event handler,
+        //   so the first time it loads, e will be undefined. After that it'll come from
+        //   an event handler.
         let {
             name: name,
             commonName: common,
-            celebrations: holidays
+            celebrations: holidays,
+            desc: desc
         } = ((e === undefined) ? month : (e.target.id == "next-month") ? nextMonth() : prevMonth());
 
         writeMonth(name, common);
-        writeHolidays(holidays)
+        writeHolidays(holidays);
+        checkForMajorHoliday(name, desc);
     }
 
     // Call it once to initilize the calendar.
@@ -51,39 +56,65 @@ import { month, nextMonth, prevMonth } from './calendar.service.js';
     // Update calendar month name.
     function writeMonth(name, common) {
         monthEl.innerHTML = name;
-        commonEl.innerHTML = common;
+        commonEl.innerHTML = (common === undefined) ? "" : common;
     }
 
     // Write holiday names to the calendar days.
     function writeHolidays(holidays) {
-        // 1. Loop thru holidays.
-        holidays.map(holiday => {
-            // 2. Loop thru days.
-            // We need to find a day that matches our holiday.
-            for (let date of dates) {
-                // 4. Reset day of holidays
-                date.nextElementSibling.innerHTML = '';
-                // 4. Some holidays last more than 1 day.
-                // These holidays are stored in an array.
-                if (Array.isArray(holiday.day)) {
-                    // 5. Loop thru multi-day array
-                    holiday.day.map(val => {
-                        // 6. Write when you have a match.
-                        if (date.innerHTML == val) {
+        // Holidays may be undefined if it's a major holiday.
+        if (holidays != undefined) {
+            // 1. Loop thru holidays.
+            holidays.map(holiday => {
+                // 2. Loop thru days.
+                // We need to find a day that matches our holiday.
+                for (let date of dates) {
+                    // 4. Reset day of holidays
+                    date.nextElementSibling.innerHTML = '';
+                    // 4. Some holidays last more than 1 day.
+                    // These holidays are stored in an array.
+                    if (Array.isArray(holiday.day)) {
+                        // 5. Loop thru multi-day array
+                        holiday.day.map(val => {
+                            // 6. Write when you have a match.
+                            if (date.innerHTML == val) {
+                                date.nextElementSibling.innerHTML = `<p>${holiday.name}</p>`;
+                            }
+                        });
+                    }
+                    // 7. Holiday is a single day.
+                    else {
+                        // 8. Write holiday if it matches the day.
+                        if (date.innerHTML == holiday.day) {
                             date.nextElementSibling.innerHTML = `<p>${holiday.name}</p>`;
                         }
-                    });
-                }
-                // 7. Holiday is a single day.
-                else {
-                    // 8. Write holiday if it matches the day.
-                    if (date.innerHTML == holiday.day) {
-                        date.nextElementSibling.innerHTML = `<p>${holiday.name}</p>`;
                     }
-                }
 
-                
-            };
+                    
+                };
+            });
+        }
+    }
+
+    function checkForMajorHoliday(name, desc) {
+        let majorHolidays = ["Midwinter", "Greengrass", "Midsummer", "Shieldmeet", "Highharvestide", "The Feast of the Moon"];
+        let match = majorHolidays.filter(holiday => {
+            if (name == holiday) {
+                return name;
+            }
         });
+        console.log(match);
+        if (match.length == 1) {
+            writeMajorHoliday(name, desc);
+        }
+        else {
+            console.log('remove major holiday class');
+            document.getElementById('major-holiday-overlay').className = 'hide';
+        }
+    }
+
+    function writeMajorHoliday(name, desc) {
+        console.log('writeMajorHoliday');
+        let overlay = document.getElementById('major-holiday-overlay');
+        overlay.className = 'show';
     }
 } ());
--- js/calendar.service.js
+++ js/calendar.service.js
@@ -1,225 +1,221 @@
-let calendar = {
-    "months": [
-        {
-            "name": "Hammer",
-            "commonName": "Deepwinter",
-            "celebrations": [
-                {
-                    "day": 1,
-                    "name": "Wintershield",
-                    "desc": ""
-                }
-            ]
-        },
-        {
-            "name": "Alturiak",
-            "commonName": "The Claw of Winter",
-            "celebrations": [
-                {
-                    "day": 14,
-                    "name": "The Grand Revel",
-                    "desc": ""
-                }
-            ]
-        },
-        {
-            "name": "Ches",
-            "commonName": "The Claw of Sunsets",
-            "celebrations": [
-                {
-                    "day": 1,
-                    "name": "Rhyestertide",
-                    "desc": ""
-                },
-                {
-                    "day": 19,
-                    "name": "Fey Day",
-                    "desc": ""
-                },
-                {
-                    "day": [21,22,23,24,25,26,27,28,29,30],
-                    "name": "Fleetswake",
-                    "desc": ""
-                }
-            ]
-        },
-        {
-            "name": "Tarsahk",
-            "commonName": "The Claw of Storms",
-            "celebrations": [
-                {
-                    "day": [1,2,3,4,5,6,7,8,9,10],
-                    "name": "Waukeentide",
-                    "desc": ""
-                }
-            ]
-        },
-        {
-            "name": "Mirtul",
-            "commonName": "The Melting",
-            "celebrations": [
-                {
-                    "day": [6,7,8,9],
-                    "name": "The Plowing and Running",
-                    "desc": ""
-                }
-            ]
-        },
-        {
-            "name": "Kythorn",
-            "commonName": "The Time of Flowers",
-            "celebrations": [
-                {
-                    "day": 1,
-                    "name": "Trolltide",
-                    "desc": ""
-                },
-                {
-                    "day": 14,
-                    "name": "Guildhall Day",
-                    "desc": ""
-                },
-                {
-                    "day": 20,
-                    "name": "Dragondown",
-                    "desc": ""
-                }
-            ]
-        },
-        {
-            "name": "Flamerule",
-            "commonName": "Summertide",
-            "celebrations": [
-                {
-                    "day": 1,
-                    "name": "Founder's Day",
-                    "desc": ""
-                },
-                {
-                    "day": [3,4,5],
-                    "name": "Sornyn",
-                    "desc": ""
-                },
-                {
-                    "day": 7,
-                    "name": "Lilira's Night",
-                    "desc": ""
-                }
-            ]
-        },
-        {
-            "name": "Eleasis",
-            "commonName": "Highsun",
-            "celebrations": [
-                {
-                    "day": 1,
-                    "name": "Ahghairon's Day",
-                    "desc": ""
-                }
-            ]
-        },
-        {
-            "name": "Elient",
-            "commonName": "The Fading",
-            "celebrations": [
-                {
-                    "day": 21,
-                    "name": "Brightswords",
-                    "desc": ""
-                }
-            ]
-        },
-        {
-            "name": "Marpenoth",
-            "commonName": "Leaffall",
-            "celebrations": [
-                {
-                    "day": 3,
-                    "name": "Day of Wonders",
-                    "desc": ""
-                },
-                {
-                    "day": 7,
-                    "name": "Stoneshar",
-                    "desc": ""
-                },
-                {
-                    "day": 10,
-                    "name": "Reign of Misrule",
-                    "desc": ""
-                },
-                {
-                    "day": 15,
-                    "name": "Gods' Day",
-                    "desc": ""
-                },
-                {
-                    "day": 30,
-                    "name": "Liar's Night",
-                    "desc": ""
-                }
-            ]
-        },
-        {
-            "name": "Uktar",
-            "commonName": "The Rotting",
-            "celebrations": [
-                {
-                    "day": getFullMoon(),
-                    "name": "Selune's Hallowing",
-                    "desc": ""
-                },
-                {
-                    "day": 20,
-                    "name": "Last Sheaf",
-                    "desc": ""
-                }
-            ]
-        },
-        {
-            "name": "Nightal",
-            "commonName": "The Drawing Down",
-            "celebrations": [
-                {
-                    "day": 11,
-                    "name": "Howldown",
-                    "desc": ""
-                },
-                {
-                    "day": 20,
-                    "name": "Simril",
-                    "desc": ""
-                }
-            ]
-        }
-    ],
-    "faerun holidays": [
-        {
-            "name": "Midwinter",
-            "desc": ""
-        },
-        {
-            "name": "Greengrass",
-            "desc": ""
-        },
-        {
-            "name": "Midsummer",
-            "desc": ""
-        },
-        {
-            "name": "Shieldmeet",
-            "desc": ""
-        },
-        {
-            "name": "Highharvestide",
-            "desc": ""
-        },
-        {
-            "name": "The Feast of the Moon",
-            "desc": ""
-        }
-    ]
-};
+let calendar = [
+    {
+        "name": "Hammer",
+        "commonName": "Deepwinter",
+        "celebrations": [
+            {
+                "day": 1,
+                "name": "Wintershield",
+                "desc": ""
+            }
+        ]
+    },
+    {
+        "name": "Midwinter",
+        "desc": ""
+    },
+    {
+        "name": "Alturiak",
+        "commonName": "The Claw of Winter",
+        "celebrations": [
+            {
+                "day": 14,
+                "name": "The Grand Revel",
+                "desc": ""
+            }
+        ]
+    },
+    {
+        "name": "Ches",
+        "commonName": "The Claw of Sunsets",
+        "celebrations": [
+            {
+                "day": 1,
+                "name": "Rhyestertide",
+                "desc": ""
+            },
+            {
+                "day": 19,
+                "name": "Fey Day",
+                "desc": ""
+            },
+            {
+                "day": [21,22,23,24,25,26,27,28,29,30],
+                "name": "Fleetswake",
+                "desc": ""
+            }
+        ]
+    },
+    {
+        "name": "Tarsahk",
+        "commonName": "The Claw of Storms",
+        "celebrations": [
+            {
+                "day": [1,2,3,4,5,6,7,8,9,10],
+                "name": "Waukeentide",
+                "desc": ""
+            }
+        ]
+    },
+    {
+        "name": "Greengrass",
+        "desc": ""
+    },
+    {
+        "name": "Mirtul",
+        "commonName": "The Melting",
+        "celebrations": [
+            {
+                "day": [6,7,8,9],
+                "name": "The Plowing and Running",
+                "desc": ""
+            }
+        ]
+    },
+    {
+        "name": "Kythorn",
+        "commonName": "The Time of Flowers",
+        "celebrations": [
+            {
+                "day": 1,
+                "name": "Trolltide",
+                "desc": ""
+            },
+            {
+                "day": 14,
+                "name": "Guildhall Day",
+                "desc": ""
+            },
+            {
+                "day": 20,
+                "name": "Dragondown",
+                "desc": ""
+            }
+        ]
+    },
+    {
+        "name": "Flamerule",
+        "commonName": "Summertide",
+        "celebrations": [
+            {
+                "day": 1,
+                "name": "Founder's Day",
+                "desc": ""
+            },
+            {
+                "day": [3,4,5],
+                "name": "Sornyn",
+                "desc": ""
+            },
+            {
+                "day": 7,
+                "name": "Lilira's Night",
+                "desc": ""
+            }
+        ]
+    },
+    {
+        "name": "Midsummer",
+        "desc": ""
+    },
+    {
+        "name": "Shieldmeet",
+        "desc": ""
+    },
+    {
+        "name": "Eleasis",
+        "commonName": "Highsun",
+        "celebrations": [
+            {
+                "day": 1,
+                "name": "Ahghairon's Day",
+                "desc": ""
+            }
+        ]
+    },
+    {
+        "name": "Elient",
+        "commonName": "The Fading",
+        "celebrations": [
+            {
+                "day": 21,
+                "name": "Brightswords",
+                "desc": ""
+            }
+        ]
+    },
+    {
+        "name": "Highharvestide",
+        "desc": ""
+    },
+    {
+        "name": "Marpenoth",
+        "commonName": "Leaffall",
+        "celebrations": [
+            {
+                "day": 3,
+                "name": "Day of Wonders",
+                "desc": ""
+            },
+            {
+                "day": 7,
+                "name": "Stoneshar",
+                "desc": ""
+            },
+            {
+                "day": 10,
+                "name": "Reign of Misrule",
+                "desc": ""
+            },
+            {
+                "day": 15,
+                "name": "Gods' Day",
+                "desc": ""
+            },
+            {
+                "day": 30,
+                "name": "Liar's Night",
+                "desc": ""
+            }
+        ]
+    },
+    {
+        "name": "Uktar",
+        "commonName": "The Rotting",
+        "celebrations": [
+            {
+                "day": getFullMoon(),
+                "name": "Selune's Hallowing",
+                "desc": ""
+            },
+            {
+                "day": 20,
+                "name": "Last Sheaf",
+                "desc": ""
+            }
+        ]
+    },
+    {
+        "name": "The Feast of the Moon",
+        "desc": ""
+    },
+    {
+        "name": "Nightal",
+        "commonName": "The Drawing Down",
+        "celebrations": [
+            {
+                "day": 11,
+                "name": "Howldown",
+                "desc": ""
+            },
+            {
+                "day": 20,
+                "name": "Simril",
+                "desc": ""
+            }
+        ]
+    }
+];
 
 // Selune's Hallowing occurs on the full moon.
 // It takes place in the month of Uktar, which
@@ -236,7 +232,7 @@ function getFullMoon() {
 // This will be a call to the localStorage service eventually.
 let currentMonth = 0;
 
-export let month = calendar.months[currentMonth];
+export let month = calendar[currentMonth];
 
 function getMonth(direction) {
     switch (direction) {
@@ -247,7 +243,7 @@ function getMonth(direction) {
             currentMonth = (currentMonth == 0) ? 11 : --currentMonth;
             break;
     }
-    return calendar.months[currentMonth];
+    return calendar[currentMonth];
 }
 
 export function nextMonth() {
